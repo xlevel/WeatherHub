c27de21316f52b48bd44e6344ca4672a8377e23e
xlevel <jim@xlevel.org.uk>
2015-04-05 18:49:56 +0000

    Added initial MySql datalayer support

---
+++ WeatherHub/src/data/data.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+}());
+
+var mysql = require('mysql');
+
+module.exports = {
+	saveReading: function(sensorId, sensorType, time, value) {
+
+	},
+
+	getReadingsForSensor: function(sensorId, sensorType, startTime, endTime) {
+
+	}
+
+
+};

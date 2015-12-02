33d94c8deb041a9e7df733ca978442c5954ee59a
Jim Hurst <jim@xlevel.org.uk>
2015-12-02 21:55:25 +0000

    Created new BMP085 sensor

--- Sensors/BMP085/bmp085.js
+++ Sensors/BMP085/bmp085.js
@@ -1,1 +1,30 @@
-//
+(function() {
+	'use strict';
+}());
+
+var sensorLib = require('bmp085');
+
+module.exports = {
+
+	initialize: function (config) {
+		this.config = config;
+	},
+
+	read: function(callback) {
+		var sensor = new sensorLib();
+		var self = this;
+
+		sensor.read(function(sensorData) {
+			var data = {
+				id: self.config.id,
+				readings: [
+					{ type: "t", value: sensorData.temperature.toFixed(2) },
+					{ type: "p", value: sensorData.pressure.toFixed(2) }
+				]
+			};
+
+			callback(data);
+		});
+	}
+
+};

4948f8141eabfc2a7eab7711815fd33c689390ab
Jim Hurst <jim@xlevel.org.uk>
2016-04-07 21:15:29 +0000

    Updated tests - Might not work still

--- WeatherNode/tests/sensors-tests.js
+++ WeatherNode/tests/sensors-tests.js
@@ -43,50 +43,37 @@ describe('Sensors', function () {
                 }
             );
         });
-        
-        it('returns an empty result if no sensor is defined', function (done) { 
-            var config = { sensors: [] };
-            
-            sensors.read(config, function (result) {
-                
-                assert.notStrictEqual(result, null);
-                assert.ok(result instanceof Array);
-                assert.equal(result.length, 0);
-                done();
-            });
-        });
 
         it('returns a single result if one sensor is defined', function (done) {
             var config = { sensors: [{ id: 'sensor 1', type: './mockSensor.js' } ] };
             
             sensors.read(config, function (result) {
                 
-                assert.notStrictEqual(result, null);
-                assert.ok(result instanceof Array);
-                assert.equal(result.length, 1);
+                assert.ok(result instanceof Object);
+                assert.equal(result.id, 'sensor 1');
                 done();
             });
         });
         
-        it('returns one result for each sensor defined', function (done) { 
-            var config = { sensors: [{ id: 'sensor 1', type: './mockSensor.js' } , { id: 'sensor 2', type: './mockSensor.js' } ] };
+        // it('returns one result for each sensor defined', function (done) { 
+        //     var config = { sensors: [{ id: 'sensor 1', type: './mockSensor.js' } , { id: 'sensor 2', type: './mockSensor.js' } ] };
             
-            sensors.read(config, function (result) {
+        //     sensors.read(config, function (result) {
                 
-                assert.equal(result.length, 2);
-                done();
-            });
-        });
+        //         assert.equal(result.length, 2);
+        //         done();
+        //     });
+        // });
         
-        it('the result contains the sensors id', function (done) {
-            var config = { sensors: [ { id: 'sensor 1', type: './mockSensor.js' } ] };
+        // it('the result contains the sensors id', function (done) {
+        //     var config = { sensors: [ { id: 'sensor 1', type: './mockSensor.js' } ] };
             
-            sensors.read(config, function (result) {
+        //     sensors.read(config, function (result) {
                 
-                assert.equal(result[0].id, 'sensor 1');
-                done();
-            });
-        });
+        //         assert.equal(result[0].id, 'sensor 1');
+        //         done();
+        //     });
+        // });
     });
 });
 

dd969d099e23e11c0f276e079a63b057330ba18e
xlevel <jim@xlevel.org.uk>
2017-03-22 21:20:05 +0000

    Refactor module structure and make use of new JS features.

--- Sensors/DHT22/dht22.js
+++ Sensors/DHT22/dht22.js
@@ -1,28 +1,25 @@
-(function() {
-	'use strict';
-}());
+'use strict';
 
-var sensorLib = require('node-dht-sensor');
+const sensorLib = require('node-dht-sensor');
 
-var DHT22Sensor = function(config) {
-  this.config = config;
-  sensorLib.initialize(22, config.settings.pin);
-};
+class DHT22Sensor{
+    constructor(config) {
+        this.config = config;
+        sensorLib.initialize(22, config.settings.pin);
+    }
 
-DHT22Sensor.prototype = {
-    read: function (callback) {
-		var reading = sensorLib.read();
-
-		var data = {
-			id: this.config.id,
+    read(callback) {
+        const reading = sensorLib.read();
+        const data = {
+            id: this.config.id,
             readings: [
                 { type: "t", value: reading.temperature.toFixed(2) },
                 { type: "h", value: reading.humidity.toFixed(2) }
-			]
-		};
+            ]
+        };
 
-		callback(data);
+        callback(data);
     }
-};
+}
 
 module.exports = DHT22Sensor;

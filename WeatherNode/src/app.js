09c5c9938cf1a36e3b42b358714a1d5499651f51
xlevel <jim@xlevel.org.uk>
2017-03-23 20:56:29 +0000

    Remove commented out code

--- WeatherNode/src/app.js
+++ WeatherNode/src/app.js
@@ -1,19 +1,13 @@
-﻿(function () {
-    'use strict';
-}());
+'use strict';
 
 const sensors = require('./sensors/sensors.js'),
     dataAccess = require('./data/data.js'),
     config = require('./config.json');
 
-//var dataAccess = new DataAccess();
-//dataAccess.initialize(config);
-
 setInterval(function () {
     sensors.read(config, function (result) {
-        //console.log(result);
+        console.log(result);
         dataAccess.save(config, result);
-        //dataAccess.save(result);
     });
 }, config.interval);
 

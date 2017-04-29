fa9ae255af1049a56acaef77edb242f20b9a26f8
xlevel <jim@xlevel.org.uk>
2017-04-29 20:48:48 +0000

    Converted BMP085 driver to the new format

--- Sensors/BMP085/bmp085.js
+++ Sensors/BMP085/bmp085.js
@@ -1,31 +1,26 @@
-(function() {
-	'use strict';
-}());
+'use strict';
 
-var sensorLib = require('bmp085');
+const sensorLib = require('bmp085');
 
-var BMP085Sensor = function(config) {
-  this.config = config;  
-};
-
-BMP085Sensor.prototype = {
-    read: function (callback) {
-		var sensor = new sensorLib();
-		var self = this;
-
-		sensor.read(function(sensorData) {
-			var data = {
-				id: self.config.id,
-				readings: [
-					{ type: "t", value: sensorData.temperature.toFixed(2) },
-					{ type: "p", value: sensorData.pressure.toFixed(2) }
-				]
-			};
-
-			callback(data);
-		});
+class BMP085Sensor{
+    constructor(config) {
+        this.config = config;
     }
-};
+
+    read(callback) {
+        const sensor = new sensorLib();
+	sensor.read((sensorData) => {
+            const data = {
+                id: this.config.id,
+                readings: [
+                    { type: "t", value: sensorData.temperature.toFixed(2) },
+                    { type: "p", value: sensorData.pressure.toFixed(2) }
+                ]
+            };
+            callback(data);
+        });
+    }
+}
 
 module.exports = BMP085Sensor;
 
